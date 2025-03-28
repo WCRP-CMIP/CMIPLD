a0d32171ab4a864e45d9a2d4363628516cf5bd4a
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-03-28 21:33:22 +0000

    development

--- cmipld/utils/git/git_core.py
+++ cmipld/utils/git/git_core.py
@@ -38,3 +38,39 @@ def url2io(github_repo_url, branch='main', path_base=''):
         github_pages_url += '/'
 
     return github_pages_url
+
+
+def io2repo(github_pages_url):
+
+    username, repo_name, path = extract_repo_info(github_pages_url)
+    base_url = f'https://github.com/{username}/{repo_name}.git'
+
+    return base_url
+
+
+def url2io(github_repo_url, branch='main', path_base=''):
+    print('make test for url2io')
+
+    if '/tree/' in github_repo_url:
+        # Regex to extract username, repo name, and path from GitHub repo URL
+        pattern = rf"https://github\.com/(?P<username>[^/]+)/(?P<repo_name>[^/]+)/tree/{branch}/{path_base}(?P<path>.*)"
+
+    else:
+        pattern = rf"https://github\.com/(?P<username>[^/]+)/(?P<repo_name>[^/]+)"
+
+    match = re.match(pattern, github_repo_url)
+
+    if not match:
+        raise ValueError("Invalid GitHub repository URL format.")
+
+    username = match.group("username")
+    repo_name = match.group("repo_name")
+    path = match.groupdict().get("path", "").strip('/')
+
+    github_pages_url = f"https://{username.lower()}.github.io/{repo_name}/{path}/"
+    if github_pages_url[-2:] == '//':
+        github_pages_url = github_pages_url[:-1]
+    elif github_pages_url[-1] != '/':
+        github_pages_url += '/'
+
+    return github_pages_url
