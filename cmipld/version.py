a21235bbd60d15f0e7be20e4fafc1bf2c39e6736
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-06-10 14:39:25 +0100

    readme

--- cmipld/version.py
+++ cmipld/version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.5.dev42+gbabe60c.d20250522'
-__version_tuple__ = version_tuple = (0, 0, 5, 'dev42', 'gbabe60c.d20250522')
+__version__ = version = '0.0.5.dev43+g823aaca.d20250610'
+__version_tuple__ = version_tuple = (0, 0, 5, 'dev43', 'g823aaca.d20250610')
