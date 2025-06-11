72fa4141a99001feaae112b94988f3f0ca577a58
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-06-11 12:03:49 +0100

    offline

--- cmipld/version.py
+++ cmipld/version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.5.dev44+g5f7dbd4.d20250610'
-__version_tuple__ = version_tuple = (0, 0, 5, 'dev44', 'g5f7dbd4.d20250610')
+__version__ = version = '0.0.5.dev51+ge8de103.d20250611'
+__version_tuple__ = version_tuple = (0, 0, 5, 'dev51', 'ge8de103.d20250611')
