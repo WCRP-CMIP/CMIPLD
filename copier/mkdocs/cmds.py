bd8d7c81dc92436395dedbfb26832f54f4b7749c
Dan Ellis <daniel.ellis@ext.esa.int>
2025-08-04 21:44:10 +0100

    Documentation template and expansion. (#30)
    * fixed readme generation
    
    * fixed location repo info updates
    
    * summary update
    
    * README page hook
    
    * stats graph fix
    
    * mkdocs template tweaks
    
    * expand correction

---
+++ copier/mkdocs/cmds.py
@@ -0,0 +1,4 @@
+python /Users/daniel.ellis/WIPwork/CMIP-LD/copier/mkdocs/auto-setup.py --no-confirm --force-overwrite
+
+mkdocs build -f .src/mkdocs/mkdocs.yml 
+mkdocs serve -f .src/mkdocs/mkdocs.yml
