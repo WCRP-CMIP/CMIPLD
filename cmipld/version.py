735871d7e2e93607b535226e5af76c683c873d77
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-10-23 13:05:44 +0100

    development

--- cmipld/version.py
+++ cmipld/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.7.dev136+gc034d3c47.d20251023'
-__version_tuple__ = version_tuple = (0, 0, 7, 'dev136', 'gc034d3c47.d20251023')
+__version__ = version = '0.0.7.dev138+g8e316725c.d20251023'
+__version_tuple__ = version_tuple = (0, 0, 7, 'dev138', 'g8e316725c.d20251023')
 
-__commit_id__ = commit_id = 'gc034d3c47'
+__commit_id__ = commit_id = 'g8e316725c'
