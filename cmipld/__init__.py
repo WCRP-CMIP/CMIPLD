04e05acd61f38ccd4a35e31bcffdcad9a7d87adb
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-11-18 15:51:04 +0000

    development

--- cmipld/__init__.py
+++ cmipld/__init__.py
@@ -1,25 +1,66 @@
 # -*- coding: utf-8 -*-
 from .locations import *
-# from pyld import jsonld
-
-# from .utils.server_tools.offline import LD_server
-
-# remap the requests links using prefixe mapping
-from .utils.server_tools.monkeypatch_requests import RequestRedirector
-redir = RequestRedirector({}, mapping)
+from jsonld_recursive import LdrClient
+import os
+import time
 
 
-from .utils.server_tools.loader import Loader
-# Don't import everything from utils to avoid circular dependencies
-# Import specific items as needed
-from .utils.io import *
-from .utils.write06 import *
-from .utils.jsontools import *
-from .utils import git 
-from .utils.extract import *
+_redirect_mapping = {}
+for k,v in mapping.items():
+    _redirect_mapping[f'{k}:*'] = v+'${rest}'
+
+print("Initializing LDR client...", flush=True)
+
+client = LdrClient(
+                auto_start_server=True,
+                timeout=10, max_retries=5,
+                mappings=_redirect_mapping
+                )
+
+print("LDR client initialized.", flush=True)
+
+if not client._is_server_running():
+    raise RuntimeError("There was a problem starting the JSON-LD Recursive server.")
+
+def get(url, depth=3):
+    return client.compact(url, depth=depth)
 
 
-loader = Loader(tries=3)
+def local_mapping_using_prefix(prefix,path):
+    # get all mappings
+    mappings = client.get_mappings()
+    # use the existing mapping for the prefix to create a new mapping
+    url = mappings[f'{prefix}:*'].replace('${rest}', '')
+    mappings[f'{url}*'] = path + '${rest}'
+    # update mappings
+    client.set_mappings(mappings)
+    print(f"Added mapping: {url} -> {path}")
+    
+    
+def map_current(prefix):
+    cwd = os.getcwd()
+    local_mapping_using_prefix(prefix, cwd+'/')
+
+# # from pyld import jsonld
+
+# # from .utils.server_tools.offline import LD_server
+
+# # remap the requests links using prefixe mapping
+# from .utils.server_tools.monkeypatch_requests import RequestRedirector
+# redir = RequestRedirector({}, mapping)
+
+
+# from .utils.server_tools.loader import Loader
+# # Don't import everything from utils to avoid circular dependencies
+# # Import specific items as needed
+# from .utils.io import *
+# from .utils.write06 import *
+# from .utils.jsontools import *
+# from .utils import git 
+# from .utils.extract import *
+
+
+# loader = Loader(tries=3)
 
 
 
@@ -35,17 +76,4 @@ loader = Loader(tries=3)
 #     module = importlib.import_module(module.__name__)
 #     importlib.reload(module)
 #     print('Reloaded', module)
-
-
-
-def expand(u):
-    return jsonld.expand(resolve_url(u))
-
-
-def getall(l):
-    '''
-    Get multiple items
-    '''
-    assert isinstance(l, list)
-    return [expand(a) for a in l]
 
--- cmipld/version.py
+++ cmipld/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.7.dev238+gdbba8acac.d20251113'
-__version_tuple__ = version_tuple = (0, 0, 7, 'dev238', 'gdbba8acac.d20251113')
+__version__ = version = '0.0.7.dev271+gadb1c973b.d20251118'
+__version_tuple__ = version_tuple = (0, 0, 7, 'dev271', 'gadb1c973b.d20251118')
 
-__commit_id__ = commit_id = 'gdbba8acac'
+__commit_id__ = commit_id = 'gadb1c973b'
